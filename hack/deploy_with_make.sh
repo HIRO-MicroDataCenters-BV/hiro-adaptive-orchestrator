c457e459a2db2b0ebc8ef92297c0b9af50aeb636
S S Krishna Vutukuri <sskrishnavutukuri@Krishnas-MacBook-Pro-M4.local>
2026-02-21 01:50:14 +0530

    Update the c;luste

--- hack/deploy_with_make.sh
+++ hack/deploy_with_make.sh
@@ -28,20 +28,34 @@ echo "Installing CRDs..."
 make install
 
 echo ""
-echo "Deploying operator..."
-make deploy
-
+echo "Building operator image..."
+make build
+    
 echo ""
 echo "Verifying deployment..."
 kubectl get crd orchestrationprofiles.orchestration.hiro.io
 
-# echo ""
-# echo "Applying sample OrchestrationProfile"
-# kubectl apply -f config/samples/orchestration_v1alpha1_orchestrationprofile.yaml
+echo ""
+echo "Applying sample OrchestrationProfile"
+kubectl apply -f config/samples/orchestration_v1alpha1_orchestrationprofile.yaml
 
-# echo ""
-# echo "Verify CRD resource is created..."
-# kubectl get orchestrationprofiles
+echo ""
+echo "Verify CRD resource is created..."
+kubectl get orchestrationprofiles
+
+echo ""
+echo "Deploying operator..."
+make deploy
 
 echo ""
 echo "Deployment completed successfully."
+
+# echo ""
+# echo "Running operator locally..."
+# make run
+
+echo ""
+echo "Describe the created OrchestrationProfile resource..."
+kubectl get orchestrationprofiles -o yaml
+
+# Still working on deploy the operator pod in the Kubernetes cluster (instead of running locally)
